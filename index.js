f97fe1d1abbebdf79755c2549714b2c912e11998
espitiaandres <espitiaandres123@gmail.com>
2020-09-07 17:51:34 -0400

    modified user join message

--- index.js
+++ index.js
@@ -24,30 +24,24 @@ io.on('connection', (socket) => {
 
         if (error) return callback(error);
 
-        socket.emit('message', { user: "admin", text: `${user.name}, welcome to the room ${user.room}` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
+        socket.emit('message', { user: "admin", text: `${user.name}, welcome to the ${user.room} chat room!` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined! :)` });
         socket.join(user.room);
-
         io.to(user.room).emit('roomData', { room: user.room , users: getUsersInRoom(user.room)})
-
         callback();
     });
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
-
         io.to(user.room).emit('message', { user: user.name, text: message });
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
-
         callback();
     });
 
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
-
         if (user) {
-            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left. :(` })
         }
     })
 })
