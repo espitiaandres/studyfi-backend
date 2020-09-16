e312976fa6588155243f1637f2429ce608ca9c1b
espitiaandres <espitiaandres123@gmail.com>
2020-09-15 22:12:54 -0400

    fixing 503 error

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('*', (req, res) => {
-    res.send('server is up and running here...proxy eminem?????');
+    res.send('server is up and running here...proxy blink182?????');
 })
 
 module.exports = router;
