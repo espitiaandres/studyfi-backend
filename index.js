fa497d39dbfb6a452d0c75b4bb244351bf68c4f8
espitiaandres <espitiaandres123@gmail.com>
2020-09-15 21:19:29 -0400

    adding credentials to cors()

--- index.js
+++ index.js
@@ -14,22 +14,11 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(router);
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Access to XMLHttpRequest at 'https://react-chat-app-back-end.herokuapp.com/socket.io/?EIO=3&transport=polling&t=NIKToPF&sid=y816dlf1JzozlBL5AAAD' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
     next();
   });
 
--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('*', (req, res) => {
-    res.send('server is up and running here...!');
+    res.send('server is up and running here...dua lipa');
 })
 
 module.exports = router;
