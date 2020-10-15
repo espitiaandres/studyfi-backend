e82f7ddfec152b6fae53a2ef8c06b2f3d9b61b40
espitiaandres <espitiaandres123@gmail.com>
2020-10-14 20:20:32 -0400

    changing display message

--- index.js
+++ index.js
@@ -89,4 +89,6 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`);
+});
